8a806b32be0c62ee9c4672bde12c2a99426fab57
paras-017 <paraschauhan0017@gmail.com>
2023-06-27 07:22:38 +0530

    organizing reactNative app

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
+import FlatCards from './components/FlatCards'
 
 const App = () => {
   return (
     <SafeAreaView>
       <Text>Hello World</Text>
-      
+      <FlatCards/>
     </SafeAreaView>
   )
 }
