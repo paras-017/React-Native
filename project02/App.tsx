8f0fe106357658a0ff2a7c18b2dd19bd083f3931
paras-017 <paraschauhan0017@gmail.com>
2023-06-28 07:49:34 +0530

    02 boilerplate for password

--- project02/App.tsx
+++ project02/App.tsx
@@ -4,18 +4,25 @@ import React, { useState } from 'react'
 //Form Validation
 import * as Yup from 'yup'
 
+// const PasswordSchema = Yup.object().shape({
+//   passwordLength: Yup.number().min(5,'Minimum length should be 5').max(20,'Maximum length should be 5').required('Length is required')
+// })
+
 const PasswordSchema = Yup.object().shape({
-  passwordLength: Yup.number().min(5,'Minimum length should be 5').max(20,'Maximum length should be 5').required('Length is required')
+  passwordLength: Yup.number().min(5,'Minimum should be 5').max(20,'Maximum should be 5').required('Length is required')
 })
 
-
 const App = () => {
-  const [passwordLength, setPasswordLength] = useState('')
+  const [password, setPassword] = useState('')
   const [isPassGenerated, setIsPassGenerated] = useState(false)
   const [lowerCase, setLowerCase] = useState(true)
   const [upperCase, setUpperCase] = useState(false)
   const [numbers, setNumbers] = useState(false)
   const [setsymbols, setSetsymbols] = useState(false)
+
+  const generatePasswordString = (passwordLength:number) => {}
+  const createPassword = (characters:string, passwordLength:number)=>{}
+  const resetPassword = () => {}
 
   return (
     <View>
