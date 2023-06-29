fb8010af386f33b81dced3537fda3bd13e8f90a0
paras-017 <paraschauhan0017@gmail.com>
2023-06-29 18:14:00 +0530

    .

--- appWrite_native/AppwriteAuth/src/appwrite/service.ts
+++ appWrite_native/AppwriteAuth/src/appwrite/service.ts
@@ -3,8 +3,8 @@ import Config from 'react-native-config'
 import Snackbar from 'react-native-snackbar'
 
 const appwriteClient = new Client()
-const APPWRITE_PROJECT_ID:string = Config.APPWRITE_PROJECT_ID!
 const APPWRITE_ENDPOINT:string = Config.APPWRITE_ENDPOINT!
+const APPWRITE_PROJECT_ID:string = Config.APPWRITE_PROJECT_ID!
 
 type CreateUserAccount = {
     email: string,
