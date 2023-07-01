ab5f76b32bc0344f698e95bf38cbe4a3f4b96ed7
paras-017 <paraschauhan0017@gmail.com>
2023-07-01 15:54:24 +0530

    project05 currencyButton compomnent created

---
+++ project06-(project06-(currency convertor))/currencyConvertor/src/components/CurrencyButton.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+//PropsWithChildren- actually stops you from passing anything which looks like a string but actually a particular data type that is being passed you can always avoid this but in case you want a hundred percent surety that whatever the data you are passing in this component that should be of some particular type
+ import type { PropsWithChildren } from 'react'
+
+ type CurrencyButtonProps = PropsWithChildren<{
+  name: string,
+  flag: string,
+ }>
+
+const CurrencyButton = ({flag,name}: CurrencyButtonProps):JSX.Element=> {
+  return (
+    <View style={styles.buttonContainer}>
+      <Text style={styles.flag}>{flag}</Text>
+      <Text style={styles.country}>{name}</Text>
+    </View>
+  )
+}
+
+export default CurrencyButton
+
+const styles = StyleSheet.create({
+  buttonContainer:{
+    alignItems: 'center',
+  },
+  flag:{
+    fontSize:28,
+    color:'#ffffff',
+    marginBottom:4
+  },
+  country:{
+    fontSize:18,
+    color:'#2d3436',
+    marginBottom:4
+  }
+})
