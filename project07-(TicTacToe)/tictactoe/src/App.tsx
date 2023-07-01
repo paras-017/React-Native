1cdf08aa1a2728078a19538ae83783da173f55fe
paras-017 <paraschauhan0017@gmail.com>
2023-07-01 21:19:45 +0530

    logic for ticTacToe completed

--- project07-(TicTacToe)/tictactoe/src/App.tsx
+++ project07-(TicTacToe)/tictactoe/src/App.tsx
@@ -1,5 +1,6 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
+import Snackbar from 'react-native-snackbar'
 
 const App = ():JSX.Element => {
   const [isCross, setIsCross] = useState(false)
@@ -66,6 +67,27 @@ const App = ():JSX.Element => {
     setGameWinner('Draw game... ⌛️');
   }
 }
+const onChangeItem = (itemNumber:number) => {
+  if(gameWinner){
+    return Snackbar.show({
+      text: gameWinner,
+      backgroundColor: 'green',
+      textColor: '#ffffff',
+    })
+  }
+  if(gameState[itemNumber] === 'empty'){
+    gameState[itemNumber] = isCross? 'cross' : 'circle'
+    setIsCross(!isCross)
+  }else{
+    return Snackbar.show({
+      text:'Position is already filled',
+      backgroundColor:'red',
+      textColor: '#FFF',
+    })
+  }
+  checkIsWinner()
+    
+}
 
   return (
     <SafeAreaView>
@@ -80,4 +102,74 @@ const App = ():JSX.Element => {
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  playerInfo: {
+    height: 56,
+
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginVertical: 12,
+    marginHorizontal: 14,
+
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.2,
+    shadowRadius: 1.5,
+  },
+  gameTurnTxt: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+  },
+  playerX: {
+    backgroundColor: '#38CC77',
+  },
+  playerO: {
+    backgroundColor: '#F7CD2E',
+  },
+  grid: {
+    margin: 12,
+  },
+  card: {
+    height: 100,
+    width: '33.33%',
+
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    borderWidth: 1,
+    borderColor: '#333',
+  },
+  winnerInfo: {
+    borderRadius: 8,
+    backgroundColor: '#38CC77',
+
+    shadowOpacity: 0.1,
+  },
+  winnerTxt: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+    textTransform: 'capitalize',
+  },
+  gameBtn: {
+    alignItems: 'center',
+
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 36,
+    backgroundColor: '#8D3DAF',
+  },
+  gameBtnText: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    fontWeight: '500',
+  },
+});
