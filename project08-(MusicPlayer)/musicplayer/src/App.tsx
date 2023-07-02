87013a526c2697679d645edab69f485c1f198b87
paras-017 <paraschauhan0017@gmail.com>
2023-07-02 12:32:22 +0530

    spotify completed

--- project08-(MusicPlayer)/musicplayer/src/App.tsx
+++ project08-(MusicPlayer)/musicplayer/src/App.tsx
@@ -1,15 +1,49 @@
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useState, useEffect} from "react"
 
-const App = ():JSX.Element => {
+import type {PropsWithChildren} from 'react';
+import {  ActivityIndicator,  SafeAreaView,  StatusBar,  StyleSheet,  View,} from 'react-native';
+
+import {setupPlayer, addTrack} from "../musicPlayerServices"
+import MusicPlayer from "./screens/MusicPlayer";
+
+
+function App(): JSX.Element {
+  const [isPlayerReady, setIsPaylerReady] = useState(false)
+
+  async function setup(){
+    let isSetup = await setupPlayer()
+
+    if (isSetup) {
+      await addTrack()
+    }
+
+    setIsPaylerReady(isSetup)
+  }
+
+  useEffect(() => {
+    setup()
+  }, [])
+  
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    )
+  }
+
   return (
-    <SafeAreaView>
-      <StatusBar/>
-      <Text>Testing seems ok</Text>
-    </SafeAreaView>
-  )
+    <View style={styles.container}>
+      <StatusBar barStyle={"light-content"} />
+      <MusicPlayer />
+    </View>
+  );
 }
 
-export default App
+const styles = StyleSheet.create({
+  container:{
+    flex: 1
+  }
+});
 
-const styles = StyleSheet.create({})
+export default App;
