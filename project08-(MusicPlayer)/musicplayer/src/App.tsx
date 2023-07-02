c88551c2191e1370868588cb1ea181ef674fcc87
paras-017 <paraschauhan0017@gmail.com>
2023-07-02 11:09:04 +0530

    p08 music player baseStructure added

---
+++ project08-(MusicPlayer)/musicplayer/src/App.tsx
@@ -0,0 +1,15 @@
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = ():JSX.Element => {
+  return (
+    <SafeAreaView>
+      <StatusBar/>
+      <Text>Testing seems ok</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
