68cb5d2f0a8d5dda4d88e8dc88d2137c405dab16
paras-017 <paraschauhan0017@gmail.com>
2023-07-02 14:01:04 +0530

    navigators choosed and configured the app for it

--- project09-(navigation)/navigation/src/App.tsx
+++ project09-(navigation)/navigation/src/App.tsx
@@ -1,15 +1,27 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import {SafeAreaView,StatusBar,StyleSheet, Text} from 'react-native';
+import Home from './screens/Home';
+import Details from './screens/Details';
 
-
+export type RootStackParamList = {
+  Home: undefined;
+  Details:{productId:string}
+};
+// we ahave multiple type fo navigator such as Stack, Drawer  we are explicitly mentioning that I want to use the stack Navigator but as I'll move on to different screen these screen may expect some data to be passed on to him.These data what type of data that they can expect ? that's why  we have created this RootStackParamList
+const Stack = createNativeStackNavigator<RootStackParamList>()
 
 function App(): JSX.Element {
   
   return (
-    <SafeAreaView >
-     <StatusBar/>
-     <Text>Hello There</Text>
-    </SafeAreaView>
+    <NavigationContainer >
+       {/* Here we are using Stack navigator */}
+       <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name='Details' component={Details} options={{title:"Product detail"}}/>
+            <Stack.Screen name='Home' component={Home} options={{title:"Trending products"}}/>
+       </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
