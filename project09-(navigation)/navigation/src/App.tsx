b6eb17f39464ee0846998cc4ddb12a6e42b33ef0
paras-017 <paraschauhan0017@gmail.com>
2023-07-02 13:33:54 +0530

    screen page added

---
+++ project09-(navigation)/navigation/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {SafeAreaView,StatusBar,StyleSheet, Text} from 'react-native';
+
+
+
+function App(): JSX.Element {
+  
+  return (
+    <SafeAreaView >
+     <StatusBar/>
+     <Text>Hello There</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
